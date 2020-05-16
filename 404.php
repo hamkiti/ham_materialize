cdd6c90ff19b977b772be64dc9c87e06a9acddbe
はむ吉 <hamkiti8+githab@gmail.com>
2020-05-16 17:09:33 +0900

    Update 404.php

--- 404.php
+++ 404.php
@@ -9,52 +9,25 @@
 
 get_header();
 ?>
+<div class="page-template-full-width">
+	<div id="primary" class="content-area col l9 m12 s12" data-tmpl="<?php echo basename(__FILE__, '.php');?>">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ham_materialize' ); ?></h1>
-				</header><!-- .page-header -->
+			<article class="error-404 not-found">
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ham_materialize' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ham_materialize' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$ham_materialize_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ham_materialize' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ham_materialize_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<div class="page-content" style="text-align:center;">
+					
+					お探しのページが見つかりません。<br>
+					ご指定のページは、削除されたか移動した可能性がございます。<br>
+					<br>
+					<br>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">TOPページに戻る</a>
 				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+			</article><!-- .error-404 -->
 
-		</main><!-- #main -->
+
 	</div><!-- #primary -->
-
+</div>
 <?php
 get_footer();
